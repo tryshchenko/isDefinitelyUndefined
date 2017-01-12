ddf4758637f6bd078c2aa495b90f13bd5dc4dfc3
Oleksandr Tryshchenko <ensaierwa@gmail.com>
2017-01-12 11:09:14 +0200

    Basic implementation

---
+++ isDefinetelyUndefined.js
@@ -0,0 +1,70 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tryshchenko Oleksandr @ensaier
+*/
+var isDefinetelyUndefined = (function() {
+	'use strict';
+
+	/**
+	 * Checks is the value looks like undefined, but not mandatory undefined
+	 * @param  {any}  something
+	 * @return {Boolean} 
+	 */
+	var isApproximtelyUndefined = function(something) {
+		return something == null;
+	}
+
+	/**
+	 * Predicts is value undefined or not
+	 * @param  {any}  something
+	 * @return {Boolean} 
+	 */
+	var isItProbablyUndefined = function(something) {
+		return something === undefined;
+	}
+
+	/**
+	 * Check is value an undefined for hesitators
+	 * @param  {any}  something
+	 * @return {Boolean} 
+	 */
+	var isItGoingToBeUndefined = function(something) {
+		return typeof(something) === 'undefined';
+	}
+
+	/**
+	 * Check is undefined for hipsters
+	 * @param  {any}  something
+	 * @return {Boolean} 
+	 */
+	var isItReallyUndefined = function(something) {
+		return something === void 0;
+	}
+
+	/**
+	 * Check is value an undefined for nuts
+	 * @param  {any}  something
+	 * @return {Boolean} 
+	 */
+	var isPrettySureUndefined = function(something) {
+		return something + [] === 'undefined';
+	}
+
+	/**
+	 * Check is value an undefined for hesitators
+	 * @param  {any}  something
+	 * @return {Boolean} 
+	 */
+	var isItAbsolutelyUndefined = function(something) {
+		return something === (function(){})();
+	}
+
+	return function(something) {
+		return isApproximtelyUndefined(something)
+			&& isItProbablyUndefined(something)
+			&& isItGoingToBeUndefined(something)
+			&& isItReallyUndefined(something)
+			&& isPrettySureUndefined(something)
+			&& isItAbsolutelyUndefined(something);
+	}
+})();
